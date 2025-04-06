6efd364468bd32ddddd37dcfb1876dee7081baaa
xnpltn <hi@xnpltn.me>
2025-04-06 17:23:55 +0200

    chad

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -6,5 +6,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>dl", function() vim.diagnostic.open_float() end, { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
